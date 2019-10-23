461ec7fc37011a2fd0f907b3fdc1c72535379d45
vinay <vsnaraya@ucsc.edu>
2019-10-23 00:18:11 -0700

    added login happy path

---
+++ planted/lib/login/login.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:planted/plantsInfo/plantsPage.dart';
+
+//only login page for now
+
+class LoginPage extends StatefulWidget {
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final usernameController = new TextEditingController();
+  final passwordController = new TextEditingController();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void signUpAndRegister() async {
+    if (_formKey.currentState.validate()) {
+      AuthResult user = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: usernameController.text, password: passwordController.text);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => PlantsPage()));
+
+      // TO DO: Add unable to register path
+    } else {
+      // credentials not good add error route;
+    }
+  }
+
+  void loginToAccount() async {
+    if (_formKey.currentState.validate()) {
+      AuthResult user = await _firebaseAuth.signInWithEmailAndPassword(
+          email: usernameController.text, password: passwordController.text);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => PlantsPage()));
+
+      // TO DO: Add unable to login path
+    } else {
+      // credentials not good add error route;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login to Planted!"),
+        ),
+        body: Form(
+            key: _formKey,
+            child: Wrap(runSpacing: 20, spacing: 20, children: <Widget>[
+              Center(
+                child: Text("Welcome to Planted!!",
+                    style: TextStyle(fontSize: 30),
+                    textAlign: TextAlign.center),
+              ),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 2.0, 15.0, 2.0),
+                  child: TextFormField(
+                      decoration: new InputDecoration(
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.green, width: 5.0),
+                          ),
+                          hintText: "username"),
+                      validator: (value) {
+                        if (EmailValidator.validate(value)) {
+                          return null;
+                        } else {
+                          return "please enter a valid email";
+                        }
+                      },
+                      controller: usernameController)),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 2.0, 15.0, 2.0),
+                  child: TextFormField(
+                      obscureText: true,
+                      decoration: new InputDecoration(
+                          labelText: 'Password',
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.green, width: 5.0),
+                          ),
+                          hintText: "password"),
+                      controller: passwordController)),
+              new Container(
+                child: new Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(70.0, 0, 30.0, 0),
+                      child: new RaisedButton(
+                        child: new Text("LogIn"),
+                        color: Colors.red[600],
+                        onPressed: loginToAccount,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(50.0, 0, 30.0, 0),
+                      child: new RaisedButton(
+                        child: new Text("SignUp"),
+                        color: Colors.blueAccent[600],
+                        onPressed: signUpAndRegister,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ])));
+  }
+}
---
+++ planted/lib/plantsInfo/plantsPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PlantsPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login to Planted!"),
+        ),
+        body: Text("Plants Page"));
+  }
+}
