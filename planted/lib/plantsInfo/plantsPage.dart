385a6341eef87eda62a2bd7d23f62372dcee9657
Daniel Wang <itsdwang@gmail.com>
2019-10-27 13:10:45 -0700

    Added simple pop-up page to add plant name, species, and light requirement

--- planted/lib/plantsInfo/plantsPage.dart
+++ planted/lib/plantsInfo/plantsPage.dart
@@ -1,11 +1,70 @@
 import 'package:flutter/material.dart';
 
 class PlantsPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Login to Planted!"),
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Name:'
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                                decoration: InputDecoration(
+                                    labelText: 'Species'
+                                )
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                                decoration: InputDecoration(
+                                    labelText: 'Light Requirement'
+                                )
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RaisedButton(
+                              child: Text("Submit"),
+                              onPressed: () {
+                                if (_formKey.currentState.validate()) {
+                                  _formKey.currentState.save();
+                                }
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          },
+          child: Text("Add a Plant"),
         ),
-        body: Text("Plants Page"));
+      ),
+    );
   }
 }
