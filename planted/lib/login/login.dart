d48ce1b1bf75574c2bf7a8a2479a147d114f6b16
Eduardo Villa <edvilla@ucsc.edu>
2019-11-10 18:26:56 -0800

    I am adding the assets folder that houses fonts and images.
    Signed-off-by: Eduardo Villa <edvilla@ucsc.edu>

--- planted/lib/login/login.dart
+++ planted/lib/login/login.dart
@@ -114,6 +114,9 @@ class _LoginPageState extends State<LoginPage> {
                   ],
                 ),
               ),
-            ])));
+            ]
+            )
+        )
+    );
   }
 }
