4b6adf354d005755b2d8118e6e71e86303538819
vinay <vsnaraya@ucsc.edu>
2019-11-22 14:58:35 -0800

    backend for reminderForm

--- planted/lib/plantsInfo/addReminders.dart
+++ planted/lib/plantsInfo/addReminders.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:planted/plantsInfo/plants.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 //only login page for now
 
 class AddRemindersPage extends StatefulWidget {
+  final Plant plant;
+  const AddRemindersPage({Key key, this.plant}) : super(key: key);
   _AddRemindersPageState createState() => _AddRemindersPageState();
 }
 
 class _AddRemindersPageState extends State<AddRemindersPage> {
   // need to show reminders for current plant and then add
   // add form button to add a reminder for plant
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final DatabaseReference databaseReference =
+      FirebaseDatabase.instance.reference();
+
   final _addReminderKey = GlobalKey<FormState>();
   final reminderNameController = new TextEditingController();
   var reminderDate;
   var reminderTime;
 
-  submitReminderForm() {
+  submitReminderForm() async {
     if (_addReminderKey.currentState.validate()) {
       _addReminderKey.currentState.save();
+      print("save");
+      final currentUser = await _firebaseAuth.currentUser();
+      await databaseReference.child("reminders").push().set({
+        'uid': currentUser.uid,
+        'plantKey': widget.plant.key,
+        'reminderName': reminderNameController.text,
+        'reminderDate': reminderDate.toString(),
+        'reminderTime': reminderTime.toString(),
+      });
+      // Navigator.of(context, rootNavigator: true).pop('dialog');
     }
+    /* save information to DB
+      {
+        key: plant.key
+        name: reminderName
+        Date: 
+        timeStart: time 
+      }
+    */
+    Navigator.of(context, rootNavigator: true).pop('dialog');
   }
 
   showReminderForm() {
