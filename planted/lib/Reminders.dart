b28c8708a0d5c761bede5f76a3688d9cfdedd9a9
vinay <vsnaraya@ucsc.edu>
2019-11-28 20:16:11 -0800

    remindersList

--- planted/lib/Reminders.dart
+++ planted/lib/Reminders.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+import './plantsInfo/reminder.dart';
+import './plantsInfo/remindersService.dart';
 
-class RemindersPage extends StatelessWidget {
+class RemindersPage extends StatefulWidget {
+  RemindersPageState createState() => new RemindersPageState();
+}
+
+class RemindersPageState extends State<RemindersPage> {
+  List<Reminder> _reminders = new List<Reminder>();
+
+  @override
+  void initState() {
+    super.initState();
+    _getRemindersForPlant();
+  }
+
+  _getRemindersForPlant() async {
+    List<Reminder> allReminders = await RemindersService.getReminders();
+    List<Reminder> filterReminders = [];
+    for (int i = 0; i < allReminders.length; i++) {
+      if (allReminders[i].isTurnedOn) {
+        filterReminders.add(allReminders[i]);
+        print(allReminders[i].isTurnedOn);
+        // print(allReminders.length);
+      }
+    }
+    setState(() {
+      _reminders = filterReminders;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(25.0),
-      child: Text(
-        'Reminders Page',
-        style: TextStyle(fontSize: 36.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("View All active reminders"),
+      ),
+      body: new ListView.builder(
+        itemCount: _reminders.length,
+        itemBuilder: (context, index) {
+          return new Card(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      new Text(_reminders[index].reminderName),
+                      new Text(_reminders[index].reminderDate),
+                      new Text(_reminders[index].reminderTime)
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
