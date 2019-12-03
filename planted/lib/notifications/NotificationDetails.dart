c2bdb6f124aebbf9a1862a4581fc23147e4fc025
Jessica <pan.jessica216@gmail.com>
2019-12-02 23:42:29 -0800

    reformatted to code style

--- planted/lib/notifications/NotificationDetails.dart
+++ planted/lib/notifications/NotificationDetails.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationDetails {
-  /// Notification details for Android
+  /// Notification details for Android.
   final AndroidNotificationDetails android;
 
-  /// Notification details for iOS
+  /// Notification details for iOS.
   final IOSNotificationDetails iOS;
 
   const NotificationDetails(this.android, this.iOS);
